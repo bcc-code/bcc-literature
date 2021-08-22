5ab415d62d3ab20d0bc61b3ba57abfcda9f37c1f
Edward Tombre <edward.tombre@gmail.com>
2021-08-22 14:03:55 +0200

    Added a night mode toggle button

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -5,7 +5,8 @@
             @click="setLanguage"
             :options="dropdownOptions"
             :button-text="currentLanguage"
-            :inner-text="$t('app.select-language')" />	
+            :inner-text="$t('app.select-language')" />
+        <a href="#" alt="Night mode" class="header__night-mode" :class="{ 'dark': nightMode }" @click.prevent="toggleNightMode"></a>
         <h4 v-if="showBackButton">{{pageName}}</h4>
         <nav v-else>
             <router-link v-for="menu in ['books', 'authors']" :key="menu" :to="{ name: menu }" :class="{ 'current' : $t(`${menu}.${menu}`) == pageName }">
@@ -19,6 +20,7 @@
 import { EventBus, Events } from '@/utils/eventBus.js';
 import LaDropdown from '@/components/la-dropdown';
 import loadjs from "loadjs";
+import { mapMutations, mapState } from 'vuex';
 
 export default {
     props: {
@@ -65,6 +67,7 @@ export default {
         }
     },
     methods: {
+        ...mapMutations('session', ['toggleNightMode']),
         setLanguage(lang) {
             this.showLanguageSelection = false;
             this.$store.commit('session/setAppLanguage', lang);
@@ -114,6 +117,7 @@ export default {
             this.$store.commit('session/setTopbarInitialized', true);
     },
     computed: {
+        ...mapState('session', ['nightMode']),
         isStandalone() {
             return window.matchMedia('(display-mode: standalone)').matches
         },
--- src/store/session.js
+++ src/store/session.js
@@ -3,19 +3,22 @@ export default {
     state: {
         userInfo: null,
         appLanguage: localStorage.getItem('appLanguage') || 'no',
+        nightMode: localStorage.getItem('nightMode') || false,
         isAuthenticated: false,
         topbarInitialized: false
     },
     mutations: {
-        setUserInfo: (state, value) => {
-            state.userInfo = value;
-        },
+        setUserInfo: (state, value) => state.userInfo = value,
         /*Authentication*/
         setAuthenticated: (state, value) => state.isAuthenticated = value,
         /*Languages*/
         setAppLanguage: (state, value) => {
             localStorage.setItem('appLanguage', value);
             state.appLanguage = value;
+        },
+        toggleNightMode: (state) => {
+            state.nightMode = !state.nightMode
+            localStorage.setItem('nightMode', state.nightMode)
         },
         setTopbarInitialized: (state, value) => {
             state.topbarInitialized = value;
