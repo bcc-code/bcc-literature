9fb1f97234d2a66fd1a6305ac323d5aa7f8673f6
Philipp Tischhauser <Philipp.Tischhauser@unisg.ch>
2021-09-14 17:32:56 +0200

    update player style

--- src/components/layout/app-text-to-speech-player.vue
+++ src/components/layout/app-text-to-speech-player.vue
@@ -1,14 +1,16 @@
 <template>
-    <div v-if="showPlayer" class="text-to-speech-player-wrapper">
-        <div class="progress-bar">
-                <div :style="{width: playingPercentage + '%'} " class="progress-bar-progress" />
-            </div>
-        <div class="player-info">
-            <h4 class="chapter-id">#{{article.chapterId}}</h4>
-            <h3 class="article-title">{{article.title}}</h3>
-            <h4 class="article-clock">{{articleClock.time}}</h4>
-        </div>      
-        <a  v-bind:class="[isPlaying ? 'pause-icon' : 'play-icon']" class="button-main play-pause-button button-circular" v-on:click="toggleSpeak(article)"> </a>
+    <div v-if="showPlayer" class="text-to-speech-player-outer">
+        <div class="text-to-speech-player-inner">
+            <div class="progress-bar">
+                    <div :style="{width: playingPercentage + '%'} " class="progress-bar-progress" />
+                </div>
+            <div class="player-info">
+                <h4 class="chapter-id">#{{article.chapterId}}</h4>
+                <h3 class="article-title">{{article.title}}</h3>
+                <h4 class="article-clock">{{articleClock.time}}</h4>
+            </div>      
+            <a  v-bind:class="[isPlaying ? 'pause-icon' : 'play-icon']" class="button-main play-pause-button button-circular" v-on:click="toggleSpeak(article)"> </a>
+        </div>
     </div>    
 </template>
 
